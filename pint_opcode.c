1a88dbf36a94c6943941ebe42bd665fc93408024
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-12-18 11:25:41 -0800

    pint_opcode.c

---
+++ pint_opcode.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Monty.h"
+
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number in the file.
+ *
+ * Description: If the stack is empty, prints an error message and exits.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+    if (!stack || !*stack)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%d\n", (*stack)->n);
+}
+
+/**
+ * main - Monty interpreter main function.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ *
+ * Description: Reads Monty instructions from a file and executes them.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: %s file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *stack = NULL;
+    char *line = NULL;
+    size_t len = 0;
+    unsigned int line_number = 0;
+
+    while (getline(&line, &len, file) != -1)
+    {
+        line_number++;
+        char *opcode = strtok(line, " \n\t\r");
+
+        if (opcode)
+        {
+            if (strcmp(opcode, "push") == 0)
+                push(&stack, line_number);
+            else if (strcmp(opcode, "pall") == 0)
+                pall(&stack, line_number);
+            else if (strcmp(opcode, "pint") == 0)
+                pint(&stack, line_number);
+            else
+            {
+                fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    fclose(file);
+
+    while (stack)
+    {
+        stack_t *temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
+    free(line);
+
+    return EXIT_SUCCESS;
+}
